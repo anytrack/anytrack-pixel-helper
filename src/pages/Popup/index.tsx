732bb093673e961fe721bc458c5db5aba242c039
zenji <canhtoannguyen60@gmail.com>
2023-05-04 16:17:49 +0700

    Update: Add roboto font

--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -5,6 +5,12 @@ import Popup from './Popup';
 import './css/index.scss';
 import {CssBaseline} from "@mui/material";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 render(
     <React.StrictMode>
         <CssBaseline>
