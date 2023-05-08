572cfc506533f7607f24d26c0c370f67ae6d0493
zenji <canhtoannguyen60@gmail.com>
2023-05-08 21:01:54 +0700

    Update: Handle array value for event

---
+++ src/pages/Popup/components/SingleEventValueText.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { Typography, styled } from '@mui/material';
+
+const SingleEventValueText = styled(Typography)(({ theme }) => ({
+    fontSize: 12,
+    lineHeight: '18px',
+}));
+
+export default SingleEventValueText;
--- src/pages/Popup/modules/SingleEventDetail.tsx
+++ src/pages/Popup/modules/SingleEventDetail.tsx
@@ -1,7 +1,9 @@
-import {Box, Typography} from '@mui/material';
+import {Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography} from '@mui/material';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import React from 'react';
 import {ATEvent} from "../../../global/entity/ATEvent";
 import {displayedValue} from "../../../global/utils";
+import SingleEventValueText from "../components/SingleEventValueText";
 
 type Props = {
     event: ATEvent
@@ -9,41 +11,77 @@ type Props = {
 
 const displayedAttributes = ['target', 'eventId', 'clickId', 'clientId', 'eventValue', 'currency',
     'trackingGroup', 'link', 'brandName', 'transactionId', 'shippingPrice', 'taxPrice', 'items']
+
+const SingleEventArrayValue = (props: any) => {
+    const {arrayValue} = props;
+    return <List>
+        {arrayValue.map((value: any, index: number) =>
+            <ListItemButton
+                sx={{
+                    px: 0
+                }}
+            >
+                <ListItemIcon
+                    sx={{
+                        minWidth: theme => theme.spacing(1.5)
+                    }}
+                >
+                    <FiberManualRecordIcon
+                        sx={{
+                            fontSize: '0.5rem',
+                            color: 'text.secondary'
+                        }}
+                    />
+                </ListItemIcon>
+                <SingleEventValueText
+                    key={value}
+                    sx={{
+                        mt: !index ? 0 : 0.5,
+                    }}
+                >
+                    {displayedValue(value)}
+                </SingleEventValueText>
+            </ListItemButton>
+        )
+        }
+    </List>
+}
 const SingleEventDetail: React.FC<Props> = ({event}) => {
     return (
         <>
             {
                 Object.keys(event)
                     .filter(attr => displayedAttributes.includes(attr))
-                    .map((key: string) => (
-                    <Box
-                        key={key}
-                        sx={{
-                            my: 1
-                        }}
-                    >
-                        <Typography
-                            sx={{
-                                fontSize: theme => theme.spacing(1.5),
-                                lineHeight: '16px',
-                                color: 'text.disabled',
-                                display: 'block',
-                                textTransform: 'capitalize'
-                            }}
-                        >
-                            {key}
-                        </Typography>
-                        <Typography
-                            className={"text-truncate"}
-                            sx={{
-                                fontSize: theme => theme.spacing(1.5),
-                                lineHeight: '18px'
-                            }}
-                        >
-                            {displayedValue(event[key as keyof ATEvent])}
-                        </Typography>
-                    </Box>
-                ))
+                    .map((key: string) => {
+                        const value = event[key as keyof ATEvent]
+                        return (
+                            <Box
+                                key={key}
+                                sx={{
+                                    my: 1
+                                }}
+                            >
+                                <Typography
+                                    sx={{
+                                        fontSize: theme => theme.spacing(1.5),
+                                        lineHeight: '16px',
+                                        color: 'text.disabled',
+                                        display: 'block',
+                                        textTransform: 'capitalize'
+                                    }}
+                                >
+                                    {key}
+                                </Typography>
+                                {Array.isArray(value) ? SingleEventArrayValue({arrayValue: value}) :
+                                    <SingleEventValueText
+                                        className={"text-truncate"}
+                                    >
+                                        {displayedValue(value)}
+                                    </SingleEventValueText>
+                                }
+                            </Box>
+                        )
+                    })
             }
         </>
     )
