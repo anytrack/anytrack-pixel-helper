26fc4e2747d11f44d027f4522dbfde5aa5d0f6cc
zenji <canhtoannguyen60@gmail.com>
2023-05-08 20:14:21 +0700

    Update: set default displayed value for event's value

--- src/global/utils.ts
+++ src/global/utils.ts
@@ -5,6 +5,7 @@ export const getActiveTab = async (): Promise<Tab | undefined> => {
     return tab
 }
 
-export const displayedValue = (value: any) => typeof value === 'string' ? value : JSON.stringify(value)
+export const displayedValue = (value: any) =>  [null, undefined].includes(value) ? 'Not set' :
+    typeof value === 'string' ? value : JSON.stringify(value)
 
 export const formatDate = (dateString: string) => new Date(dateString).toLocaleString().replace(/\//g,'-')
